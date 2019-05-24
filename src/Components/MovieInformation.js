5a2d77e2437a20fcf0d7eaadb35eb526133ea5c7
Jonathan Fraimorice <yonifra@gmail.com>
2019-05-24 22:25:40 +0300

    Missing files

---
+++ src/Components/MovieInformation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieInformation extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export default MovieInformation;
