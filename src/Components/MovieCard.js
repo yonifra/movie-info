2a3d38e62e802a6f5e0a271168440690c172c56c
Jonathan Fraimorice <yonifra@gmail.com>
2019-06-02 23:01:09 +0300

    initial css stuff

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class MovieCard extends Component {
     render() {
         return (
-            <div>
-                <div><img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${this.props.movie.poster_path}`}/></div>
-                <div>{JSON.stringify(this.props.movie.title)}</div>
-                <div><b>Synopsis:</b> {JSON.stringify(this.props.movie.overview)}</div>
-                <div><b>Released:</b> {JSON.stringify(this.props.movie.release_date)}</div>
-                <div><b>Rating:</b> {JSON.stringify(this.props.movie.vote_average)}</div>
+            <div class="wrapper">
+                <div class="box a"><img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${this.props.movie.poster_path}`}/></div>
+                <div class="box b">{JSON.stringify(this.props.movie.title)}</div>
+                <div class="box b"><b>Synopsis:</b> {JSON.stringify(this.props.movie.overview)}</div>
+                <div class="box b"><b>Released:</b> {JSON.stringify(this.props.movie.release_date)}</div>
+                <div class="box b"><b>Rating:</b> {JSON.stringify(this.props.movie.vote_average)}</div>
             </div>
         );
     }
