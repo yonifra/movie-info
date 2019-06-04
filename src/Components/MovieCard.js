968aa57632b3246695174208c36badb1d49b741b
Jonathan Fraimorice <yonifra@gmail.com>
2019-06-04 20:39:24 +0300

    Renamed wrappers

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class MovieCard extends Component {
     render() {
         return (
-            <div class="wrapper">
+            <div class="wrapper-cols">
                 <div class="box a">
                     <img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${this.props.movie.poster_path}`}/>
                 </div>
-                <div class="wrapper2">
+                <div class="wrapper-rows">
                     <div class="box b"><b>{JSON.stringify(this.props.movie.title)}</b></div>
                     <div class="box b"><b>Synopsis:</b> {JSON.stringify(this.props.movie.overview)}</div>
                     <div class="box b"><b>Released:</b> {JSON.stringify(this.props.movie.release_date)}</div>
