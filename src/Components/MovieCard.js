4fb97f87992986cd10e22a0daf04f4b7ce757c35
Jonathan Fraimorice <yonifra@gmail.com>
2019-06-04 12:47:12 +0300

    Fixed some UI stuff

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,11 +4,15 @@ class MovieCard extends Component {
     render() {
         return (
             <div class="wrapper">
-                <div class="box a"><img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${this.props.movie.poster_path}`}/></div>
-                <div class="box b">{JSON.stringify(this.props.movie.title)}</div>
-                <div class="box b"><b>Synopsis:</b> {JSON.stringify(this.props.movie.overview)}</div>
-                <div class="box b"><b>Released:</b> {JSON.stringify(this.props.movie.release_date)}</div>
-                <div class="box b"><b>Rating:</b> {JSON.stringify(this.props.movie.vote_average)}</div>
+                <div class="box a">
+                    <img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${this.props.movie.poster_path}`}/>
+                </div>
+                <div class="wrapper2">
+                    <div class="box b"><b>{JSON.stringify(this.props.movie.title)}</b></div>
+                    <div class="box b"><b>Synopsis:</b> {JSON.stringify(this.props.movie.overview)}</div>
+                    <div class="box b"><b>Released:</b> {JSON.stringify(this.props.movie.release_date)}</div>
+                    <div class="box b"><b>Rating:</b> {JSON.stringify(this.props.movie.vote_average)}</div>
+                </div>
             </div>
         );
     }
